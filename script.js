cd95a7fa3f0dedff8508d156a77c780d53722936
Miniflint <tristan.goel@gmail.com>
2024-06-02 19:25:30 +0200

    opening and closing bars

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ function toggleMenu(index) {
     const bars = document.querySelectorAll('.bar');
     const menu = bars[index].querySelector('.menu');
     const isVisible = menu.style.display === 'block';
+    barswidth = bars[index].clientWidth;
 
-    // Close all menus and reset transformations
-    bars.forEach(bar => {
-        const menu = bar.querySelector('.menu');
-        menu.style.display = 'none';
-        bar.classList.remove('expanded');
-    });
-
+    if (isVisible) {
+        bars.forEach(bar => {
+            const menu = bar.querySelector('.menu');
+            menu.style.display = 'none';
+            bar.classList.remove('expanded');
+        });
+    }
     // Open the selected menu if it was not already visible
     if (!isVisible) {
+        // Close all menus and reset transformations
         menu.style.display = 'block';
         bars[index].classList.add('expanded');
         width = document.body.offsetWidth;
-        document.documentElement.style.setProperty('--translate-x', (width - (bars.length * 100)) + "px"); // Adjust the width of the menu here
+        document.documentElement.style.setProperty('--translate-x', (width - (bars.length * barswidth)) + "px"); // Adjust the width of the menu here
     }
 }
-
-
