aedf2aaa59322bb2620629be907bed92a2a61e20
Miniflint <tristan.goel@gmail.com>
2024-06-02 18:59:33 +0200

    livio code

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMenu(index) {
+    const bars = document.querySelectorAll('.bar');
+    const menu = bars[index].querySelector('.menu');
+    const isVisible = menu.style.display === 'block';
+
+    // Close all menus and reset transformations
+    bars.forEach(bar => {
+        const menu = bar.querySelector('.menu');
+        menu.style.display = 'none';
+        bar.classList.remove('expanded');
+    });
+
+    // Open the selected menu if it was not already visible
+    if (!isVisible) {
+        menu.style.display = 'block';
+        bars[index].classList.add('expanded');
+        width = document.body.offsetWidth;
+        console.log(bars[index])
+    }
+}
+
+
